4129fd3821530055d2a9f4f618b4368369357eb5
Keith Newman <kin7@cornell.edu>
2013-02-18 17:01:37 +0000

    User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessor :password
+  attr_accessible :email, :name, :password
+  
+  validates :password, presence: true, if: "hashed_password.blank?"
   
   validates :name, presence: true,
                       length: { minimum: 4, maximum: 50 }
@@ -8,4 +11,38 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                         format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+  
+  has_many :micro_posts
+
+  before_save :encrypt_password
+                
+  def encrypt_password
+    self.salt ||= Digest::SHA256.hexdigest("--#{Time.now.to_s}- -#{email}--")
+    self.hashed_password = encrypt(password)
+  end
+
+  def encrypt(raw_password)
+    Digest::SHA256.hexdigest("--#{salt}--#{raw_password}--")
+  end
+  
+  # This function takes an email (as a string) and the plain_text_password
+  # (as the user would have typed it in a web form), and should return:
+  #   * if the email doesn't exist in the database, or the password given
+  #     does not match the password for the given user, return nil
+  #   * if the user with the given email has the password provided,
+  #     return that user.
+  # 
+  # You may wish to review the slides from lecture_3, which have references
+  # to using the "find" functions provided by active record so that you
+  # can locate the correct user in the database.
+  def self.authenticate(email, plain_text_password)
+	user = User.find_by_email(email)
+	if user == nil
+		return nil
+	elsif user.hashed_password != user.encrypt(plain_text_password)
+		return nil
+	else
+		return user
+	end
+  end
 end
