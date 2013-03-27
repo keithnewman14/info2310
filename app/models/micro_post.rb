7e055b2d11bbc86320a6b72600b53bbbae021cd1
Keith Newman <kin7@cornell.edu>
2013-03-27 18:39:00 +0000

    welcome and mentions emails

--- app/models/micro_post.rb
+++ app/models/micro_post.rb
@@ -6,5 +6,26 @@ class MicroPost < ActiveRecord::Base
   validates :content, presence: true,
                       length: { minimum: 5, maximum: 140 }
   validates :user_id, presence: true
+  
+  after_create :send_mentions_email
+  
+  def send_mentions_email
+	mentions.each do |mentioned_user|
+		UserMailer.mentioned(self, mentioned_user).deliver
+	end
+  end
+  
+  
+  def mentions
+    results = []
+	current_user = User.find_by_id(user_id)
+    content.scan(/@[^@]+@/) do |match|
+      name = match[1..match.length - 2]
+	  mentioned_user = User.find_by_name(name)
+	  results << mentioned_user if mentioned_user && mentioned_user != user
+    end
+
+    results
+  end
 
 end
